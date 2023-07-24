34e7b7f673dd5674a717af757427fca51f05abb7
sapnil sharma <sapnilsharma34@gmail.com>
2023-07-24 12:53:36 -0400

    debugged model.py

--- routes/admin.py
+++ routes/admin.py
@@ -1,6 +1,8 @@
 from flask import make_response, session, Blueprint
 from flask import Flask, render_template, redirect, current_app, url_for, flash, make_response
 
+# from models import User, get_session
+
 from models import User, get_session
 admin = Blueprint('admin', __name__)
 
@@ -10,7 +12,7 @@ def user_page():
     data=get_session().query(User).all()
 
     
-    return render_template('admin.html', data=data)
+    return render_template('admin/admin.html', data=data)
 # def dashboard():
 #     return render_template('admin/dashboard.html')
 
