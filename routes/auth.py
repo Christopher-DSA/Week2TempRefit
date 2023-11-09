1526a67c55bbee36ce7ef0918c5b048918fb3d4e
Yael Flax <yaelflax6@gmail.com>
2023-11-08 21:02:08 -0500

    add pandas to requirements removed password reset

--- routes/auth.py
+++ routes/auth.py
@@ -56,21 +56,25 @@ def logout():
 @auth.route("/forgot_password", methods=["GET","POST"])
 def forgot_password():
     if request.method == "POST":
-        email = request.form.get('username')
-        new_password = request.form.get('new_password')
+        email = request.form.get('username') # assuming username is the same as email
+        #new_password = request.form.get('new_password')  # shouldn't this be deleted ? new password only happens in reset link sent after email authenticated
         
-        if email and new_password:
-            # Use a query to find the user by email
+        if email :# removed 'and password' - because user forgot password and new password creation page is a link sent by email( if matches db)
+           
             
             # user = CRUD.get_user_by_email(email)
             users = CRUD.read(User, email=email)
            
+
             if users:
-                user=users[0]
-                
+                user=users[0]  #assuming each user has a distinct email and multiple users dont share one email
+              # add code to email user with password reset link , if it matches account in database 
+              # reset link should be a  new , secure page  if email was validated 
+
+
             #     # Use the CRUD update method to change the password
                 updated_user = CRUD.update(User, user.user_id, password=new_password)
-                return jsonify({'message': 'Password changed successfully'})
+                return jsonify({'message': 'Password reset link sent '}) # removed 'Password changed successfully' because pass reset should be on a different page
             else:
                 return jsonify({'error': 'User not found'})
 
