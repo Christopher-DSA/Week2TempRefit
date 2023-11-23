4a01ba388497df11f974eac76437d549425fdbd0
iuxo <jared.chuang28@gmail.com>
2023-11-23 15:59:50 -0800

    SDS-902-Refit-Basic Welcome Email Template Added

--- email_template/my_email.py
+++ email_template/my_email.py
@@ -5,10 +5,10 @@ try:
     msg = MIMEMultipart()
     msg['From'] = 'refit_dev@sidneyshapiro.com'
     msg['To'] = 'refit_dev@sidneyshapiro.com'
-    msg['Subject'] = 'Email Confirmation Successfull.'
+    msg['Subject'] = 'Welcome to REFit!'
     
     # Read the HTML content from your file
-    with open("email_template/my_templates/test_template.html", "r") as file:
+    with open("email_template/my_templates/welcome_template.html", "r") as file:
         html = file.read()
 
     # Attach the HTML content
