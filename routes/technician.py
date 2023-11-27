188e22961983b7cdc80d66e3472c4e5eaec039ef
Christopher Martinez Demmans <132075292+Christopher-DSA@users.noreply.github.com>
2023-11-27 07:36:36 -0500

    SDS-900-REFIT-Bug Fixes

--- routes/technician.py
+++ routes/technician.py
@@ -6,10 +6,6 @@ from functools import wraps
 import UUID_Generate
 technician = Blueprint('technician', __name__)
 
-# @technician.route("/technician/dashboard", methods=['GET', 'POST'])
-# def dashboard():
-#     return render_template('technician/dashboard.html')
-
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -23,17 +19,24 @@ def technician_required(f):
     def decorated_function(*args, **kwargs):
         # Assuming the user_id in the session is the email of the user.
         current_user_id = session.get('user_id')
-
-        
-
         current_user = CRUD.read(User,user_id=current_user_id)
         
         if not current_user or current_user.role != 'technician':
             # Either user doesn't exist, or the user is not an admin.
             return "Unauthorized", 403
-        
+
         return f(*args, **kwargs)
     return decorated_function
+
+@technician.route("/dashboardtechnician")
+@login_required
+@technician_required
+def dashboardtechnician():
+    # Render the dashboard
+    print("Rendering dashboard")
+    user_current=session.get('user_id')
+    user_first_name=CRUD.read(User_Detail,user_id=user_current).first_name
+    return render_template("technician/dashboardtechnician.html", user=user_current,user_first_name = user_first_name)
 
 @technician.route("/formtechnician", methods=["GET", "POST"])
 def formtechnician():
@@ -57,26 +60,9 @@ def formtechnician():
         new_detail=CRUD.create(User_Detail,user_id=session.get('user_id'),first_name=firstName,last_name=lastName, address=addressLine, province=province, city=city,postal_code=postalCode,telephone=phoneNumber)
         new_technician_detail=CRUD.create(Technician, ODS_licence_number=odsLicenseNumber,user_id=session.get('user_id'))
         return redirect(url_for('technician.dashboardtechnician'))
-                    
-
-        
-    return render_template("technician/formtechnician.html")
-
-
-@technician.route("/dashboardtechnician")
-@login_required
-@technician_required
-def dashboardtechnician():
-    # Render the dashboard
-    print("Rendering dashboard")
-    user_current=session.get('user_id')
-    user_first_name=CRUD.read(User_Detail,user_id=user_current).first_name
-    return render_template("technician/dashboardtechnician.html", user=user_current,user_first_name = user_first_name)
-
-
-@technician.route("/equipment_create_new_qr", methods=['GET', 'POST'])
-
-
+    else:
+        return render_template("technician/formtechnician.html")
+    
 @technician.route('/equipment/equipment_create', methods = ['GET', 'POST'])
 def equipment_create():
     print("inside equipment_create")
@@ -139,20 +125,6 @@ def equipment_create():
 def successful_add():
     print("inside successful_add")
     return render_template('equipment/equipment-linked.html')
-
-
-
-##@technician.route('/equipment/repair')
-##def repair():
- ##   return render_template('equipment/repair.html')
-
-
-
-##@technician.route('/equipment/recovery')
-
-##def recovery():
-
-   ## return render_template('equipment/recovery.html')
    
 @technician.route('/choose-qr-type', methods = ['GET'])
 def my_choose_qr_type():
@@ -177,8 +149,6 @@ def my_choose_qr_type():
             return render_template('Equipment Common/choose-qr-type.html')    
         
 
-    
-
 @technician.route('/New Cylinder/tag-linked')
 def add_qr():
     return render_template('New Cylinder/tag-linked.html')
@@ -193,32 +163,57 @@ def charge():
 
 @technician.route('/equipment/repair_ODS_Sheet')
 def repair_ODS_Sheet():
-
-
     return render_template('equipment/repair_ODS_Sheet.html')
-
-
 
 @technician.route('/equipment common/qr-scan')
 def qr_scan():
-
-
     return render_template('equipment common/qr-scan.html')
 
 @technician.route('/recovery/recovery-ods-sheet')
 def recovery_ods_sheet():
-
-
     return render_template('recovery/recovery-ods-sheet.html')
 
 @technician.route('/equipment/equipment_pages', methods = ['GET', 'POST'])
 def equipment_page():
-    
     if request.method == 'GET':
         return render_template('equipment/equipment_pages.html')
     else:
         print('error')
         return render_template('equipment/equipment_pages.html')
+
+
+##################################################
+#View History/Logs of Equipment/Cylinder Routes###
+##################################################
+#Make a selection between unit and cylinder
+@technician.route('/choose-equipment-history-type', methods = ['GET', 'POST'])
+def choose_equipment_type():
+        if request.method == 'GET':
+            return render_template('technician/select-history-type.html')
+        else:
+            print('error')
+            return render_template('technician/select-history-type.html')
+
+#selected UNIT history
+@technician.route('/select-history-type-technician-maintenance', methods = ['GET', 'POST'])
+def select_history_type_tech():
+    
+    if request.method == 'GET':
+        return render_template('equipment/maintenance_history.html')
+    else:
+        print('error')
+        return render_template('equipment/maintenance_history.html')
+
+#selected CYLINDER history
+@technician.route('/select-history-type-technician-cylinder', methods = ['GET', 'POST'])
+def select_history_type_tech_cylinder():
+    if request.method == 'GET':
+        return render_template('equipment/ODS-history.html')
+    else:
+        print('error')
+        return render_template('equipment/ODS-history.html')
+##################################################
+##################################################
     
 @technician.route('/equipment/ODS-history', methods = ['GET', 'POST'])
 def ODS_history():
@@ -238,7 +233,6 @@ def maintenance_history():
         print('error')
         return render_template('equipment/maintenance_history.html')
     
-    
 @technician.route('/equipment-info/<unique_id>', methods = ['GET', 'POST'])
 def equipment_info_page(unique_id):
     if request.method == 'GET':
@@ -257,26 +251,24 @@ def equipment_info_page(unique_id):
         tech_id = session.get('tech_id')
         #3. Render html
         return render_template('beta/equipment_info.html', data=data, tech_id = tech_id)
-        
-        # #Get foreign keys to search other tables.
-        # cylinder_refrigerant_id = data.refrigerant_id
-        # current_cylinder_type_id = data.cylinder_type_id
-        
-        # #Get name of refrigerant and the type of cylinder.
-        # refrigerant_table_lookup = CRUD.read(Refrigerant, all = False, refrigerant_id = cylinder_refrigerant_id)
-        # cylinder_type_lookup = CRUD.read(Cylinder_Type, all = False, cylinder_type_id = current_cylinder_type_id)    
-        
-        # name_data = {
-        #     "refrigerant_name": refrigerant_table_lookup.refrigerant_name,
-        #     "cylinder_type": cylinder_type_lookup.type_name
-        # }
-        
-        # return render_template("beta/cylinder_info.html", data=data, name = name_data)
-        
-        #2. Pass data to html
+
     else:
         print('error')
         return render_template('equipment-info.html')
+    
+@technician.route('/buy-qr', methods = ['GET', 'POST'])
+def buy_qr_page():
+    if request.method == 'GET':
+        return render_template('Equipment Common/placeholder-buy-qr-tag.html')
+    else:
+        print('error')
+        return render_template('equipment/maintenance_history.html')
 
+##@technician.route('/equipment/repair')
+##def repair():
+ ##   return render_template('equipment/repair.html')
+##@technician.route('/equipment/recovery')
+##def recovery():
+   ## return render_template('equipment/recovery.html')
 
         
