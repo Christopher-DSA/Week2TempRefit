142e022a0500e5e8882d347f3e6f219afa4223d6
Christopher Martinez Demmans <132075292+Christopher-DSA@users.noreply.github.com>
2024-01-22 06:53:17 -0500

    Added Service Worker

---
+++ static/sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', e => { //This event is fired when the service worker is first installed.
+  console.log('Install Chris Refit!');
+  e.waitUntil( //This promise doesn't allow the service worker to be considerd installed until the promise is resolved.
+    caches.open('static').then(cache => {
+        return cache.addAll(["https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css","https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js","images/refitLogo192.png","images/refitLogo512.png"])
+    })
+  );
+}); //service workers only get installed once in their lifecycle.
+
+self.addEventListener("fetch", e => { //This event is fired whenever the browser requests a resource.}
+    console.log('Intercepting fetch request for: ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response  || fetch(e.request); //If the resource is in the cache, return it, otherwise fetch it from the network.
+        })
+    );  
+});
